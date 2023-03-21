4cbc36090f020921ba37610f34c6bfe036e171fd
Gaspar.Saccomandi <117050233+Gaspar066@users.noreply.github.com>
2023-03-21 18:14:47 -0300

    Add files via upload

---
+++ Task3/scripts/upcomingEvents.js
@@ -0,0 +1,79 @@
+let sectionCards = document.querySelector(".sectionCards");
+let eventos = data.events
+let categories = [];
+//---------CARDS GENERATOR--------------//
+function cardsGenerator(array) {
+  let cards = ""
+  array.forEach(evento => {    
+    cards += `
+                  <div class="card">
+                    <img src=${evento.image} class="card-img-top">
+                    <div class="card-body">
+                        <h5>${evento.name}</h5>
+                          <div class="footer-card">
+                            <p>Price: $${evento.price}</p>
+                            <a href="./details.html?id=${evento._id}"class="btn btn-primary">Ver más</a>
+                          </div>
+                    </div>       
+                  </div>         
+                    `;
+    });
+    sectionCards.innerHTML = cards
+}
+  
+  let upcomingEvents = []     // creo array para filtrar eventos futuros.
+  function eventosFuturos(){      
+  const currentDate = data.currentDate        // Fecha a comparar
+  eventos.forEach(event => {
+    if (event.date >= currentDate){
+      upcomingEvents.push(event)
+    }
+  })
+  }
+  eventosFuturos(eventos);
+
+  function categorias(){
+    for (let i = 0; i < eventos.length; i++) {
+
+          if (!categories.includes(eventos[i].category)) {
+            categories.push(eventos[i].category);
+          }else null;
+    }
+
+    for (let i = 0; i < categories.length; i++) {
+      let idCategories = document.getElementById("categories");
+      let divCategory = document.createElement("div");
+      let eventCategory = `
+          <input type="checkbox" 
+          id="${categories[i]}" 
+          name="${categories[i]}" value="${categories[i]}">
+          <label for="${categories[i]}">${categories[i]}</label>
+          `;
+      divCategory.innerHTML += eventCategory;
+      idCategories.appendChild(divCategory);
+    }
+  };
+  categorias(eventos);
+  cardsGenerator(upcomingEvents);
+
+  function busqueda(){ 
+  const input = document.getElementById("searchUpcoming")
+  let dataInput;
+  const checkboxs = document.querySelectorAll("input[type='checkbox']");
+  function filtroCards(e){
+    e.preventDefault()
+    dataInput = input.value.trim().toLowerCase();
+    let checkedBoxes = Array.from(checkboxs).filter(checkbox => checkbox.checked).map(checkbox => checkbox.value.toLowerCase());
+    let checkboxes = upcomingEvents.filter(function (evento){
+    let search = evento.name.toLowerCase().includes(dataInput) && evento.description.toLowerCase().includes(dataInput);
+    let filteredCat = checkedBoxes.length == 0 || checkedBoxes.includes(evento.category.toLowerCase());
+      return search && filteredCat;
+    })
+    if (checkboxes.length > 0){
+      cardsGenerator(checkboxes)
+    }else  cardsGenerator(null); 
+    } 
+  input.addEventListener("input", filtroCards)
+  checkboxs.forEach(checkbox => checkbox.addEventListener("change", filtroCards))
+} 
+busqueda(eventos);
